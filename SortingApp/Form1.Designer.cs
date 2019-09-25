c55325af48ded4dd5ddea00d1312b3095b9655e4
jo692 <jo692@york.ac.uk>
2019-09-25 18:54:48 +0100

    Adjusted GUI.

--- SortingApp/Form1.Designer.cs
+++ SortingApp/Form1.Designer.cs
@@ -41,7 +41,7 @@
             // listDisplay
             // 
             this.listDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listDisplay.Location = new System.Drawing.Point(119, 80);
+            this.listDisplay.Location = new System.Drawing.Point(12, 37);
             this.listDisplay.Name = "listDisplay";
             this.listDisplay.Size = new System.Drawing.Size(523, 30);
             this.listDisplay.TabIndex = 0;
@@ -50,7 +50,7 @@
             // newListButton
             // 
             this.newListButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.newListButton.Location = new System.Drawing.Point(313, 116);
+            this.newListButton.Location = new System.Drawing.Point(205, 73);
             this.newListButton.Name = "newListButton";
             this.newListButton.Size = new System.Drawing.Size(124, 30);
             this.newListButton.TabIndex = 1;
@@ -62,7 +62,7 @@
             // 
             this.bubbleSortButton.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.bubbleSortButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bubbleSortButton.Location = new System.Drawing.Point(221, 195);
+            this.bubbleSortButton.Location = new System.Drawing.Point(76, 121);
             this.bubbleSortButton.Name = "bubbleSortButton";
             this.bubbleSortButton.Size = new System.Drawing.Size(131, 39);
             this.bubbleSortButton.TabIndex = 2;
@@ -74,7 +74,7 @@
             // 
             this.currentOperationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.currentOperationLabel.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.currentOperationLabel.Location = new System.Drawing.Point(119, 52);
+            this.currentOperationLabel.Location = new System.Drawing.Point(12, 9);
             this.currentOperationLabel.Name = "currentOperationLabel";
             this.currentOperationLabel.Size = new System.Drawing.Size(523, 25);
             this.currentOperationLabel.TabIndex = 3;
@@ -84,7 +84,7 @@
             // 
             this.selectionSortButton.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.selectionSortButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.selectionSortButton.Location = new System.Drawing.Point(404, 195);
+            this.selectionSortButton.Location = new System.Drawing.Point(328, 121);
             this.selectionSortButton.Name = "selectionSortButton";
             this.selectionSortButton.Size = new System.Drawing.Size(131, 39);
             this.selectionSortButton.TabIndex = 4;
@@ -96,7 +96,7 @@
             // 
             this.mergeSortButton.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.mergeSortButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mergeSortButton.Location = new System.Drawing.Point(221, 256);
+            this.mergeSortButton.Location = new System.Drawing.Point(76, 175);
             this.mergeSortButton.Name = "mergeSortButton";
             this.mergeSortButton.Size = new System.Drawing.Size(131, 39);
             this.mergeSortButton.TabIndex = 5;
@@ -107,7 +107,7 @@
             // 
             this.quickSortButton.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.quickSortButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.quickSortButton.Location = new System.Drawing.Point(404, 256);
+            this.quickSortButton.Location = new System.Drawing.Point(328, 175);
             this.quickSortButton.Name = "quickSortButton";
             this.quickSortButton.Size = new System.Drawing.Size(131, 39);
             this.quickSortButton.TabIndex = 6;
@@ -118,7 +118,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(552, 233);
             this.Controls.Add(this.quickSortButton);
             this.Controls.Add(this.mergeSortButton);
             this.Controls.Add(this.selectionSortButton);
